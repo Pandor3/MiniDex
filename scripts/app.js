6f4613d3a28e7b9bb30e76dc85773d1b1e84374d
Pandor3 <ThatDumbPhilosopher@gmail.com>
2025-06-04 14:04:18 +0200

    Ajout d'un système de filtrage par consoles et correction de syntaxe.

--- scripts/app.js
+++ scripts/app.js
@@ -1,37 +1,76 @@
 console.log('Fichier JS Chargé');
+
 fetch('./data/games.json')
     .then(response => response.json())
     .then(data => {
         console.log(data);
 
+        /* Ceci permet de trier les jeux du plus ancien au plus récent */
         data.sort((nouveau, ancien) => nouveau.year - ancien.year);
 
-    data.forEach(game => {
-        const card = document.createElement('div');
-        card.classList.add('game-card');
+        /* Ceci permet de classer les consoles dans un tableau en lisant les cartes de jeu */
+        let uniqueConsoles = [];
+        data.forEach(game => {
+            game.console.forEach(consoleName => {
+                if (!uniqueConsoles.includes(consoleName)) {
+                    uniqueConsoles.push(consoleName);
+                }
+            });
+        });
 
-        const img = document.createElement('img');
-        img.setAttribute('src', game.cover);
-        img.setAttribute('alt', game.title);
-        card.appendChild(img);
+        /* Ceci permet de mettre les consoles dans le système de filtrage */
+        const selectElement = document.getElementById('consoles-select');
+        uniqueConsoles.forEach(consoleName => {
+            const option = document.createElement('option');
+            option.textContent = consoleName;
+            option.value = consoleName;
+            selectElement.appendChild(option);
+        });
 
-        const title = document.createElement('h2');
-        title.textContent = game.title;
-        card.appendChild(title);
-
-        const year = document.createElement('p');
-        year.textContent = "Année : " + game.year;
-        card.appendChild(year);
-
-        const consoles = document.createElement('p');
-        consoles.textContent = "Consoles : " + game.console.join(", ");
-        card.appendChild(consoles);
-
-        const genres = document.createElement('p');
-        genres.textContent = "Genres : " + game.genre.join(", ");
-        card.appendChild(genres);
-
+        
         const gallery = document.getElementById('game-gallery');
-        gallery.appendChild(card);
+        
+        /* Création des cartes de jeux dynamiquement */
+        function showGames(list) {
+            list.forEach(game => {
+                const card = document.createElement('div');
+                card.classList.add('game-card');
+
+                const img = document.createElement('img');
+                img.setAttribute('src', game.cover);
+                img.setAttribute('alt', game.title);
+                card.appendChild(img);
+
+                const title = document.createElement('h2');
+                title.textContent = game.title;
+                card.appendChild(title);
+
+                const year = document.createElement('p');
+                year.textContent = "Année : " + game.year;
+                card.appendChild(year);
+
+                const consoles = document.createElement('p');
+                consoles.textContent = "Consoles : " + game.console.join(", ");
+                card.appendChild(consoles);
+
+                const genres = document.createElement('p');
+                genres.textContent = "Genres : " + game.genre.join(", ");
+                card.appendChild(genres);
+
+                gallery.appendChild(card);
+        });
+    }
+    /* Système de filtrage par consoles */
+    showGames(data);
+    selectElement.addEventListener('change', event => {
+        const selectedConsole = event.target.value;
+        gallery.innerHTML = "";
+
+        if (selectedConsole === "all") {
+            showGames(data);
+        } else {
+            const gamesFilter = data.filter(game => game.console.includes(selectedConsole));
+            showGames(gamesFilter);
+        }
     });
 });
